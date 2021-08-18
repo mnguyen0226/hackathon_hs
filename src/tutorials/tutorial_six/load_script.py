e3269a783357d5f8ed5ffb02ce488ff66a52c508
mnguyen0226 <mnguyen0226@vt.edu>
2021-08-18 18:30:30 -0400

    add img

--- src/tutorials/tutorial_six/load_script.py
+++ src/tutorials/tutorial_six/load_script.py
@@ -15,31 +15,19 @@ trained_model = DQN.load(
 )
 
 
-class TrainedRLPlayer(Gen8EnvSinglePlayer):
 
+class TrainedRLPlayer(Gen8EnvSinglePlayer):
     observation_space = Box(low=-10, high=10, shape=(10,))
     action_space = Discrete(22)
 
     def getThisPlayer(self):
-        """Get the current RL agent"""
         return self
 
     def __init__(self, *args, **kwargs):
-        """Initialize agent"""
-        Gen8EnvSinglePlayer.__init__(self)
+        Gen8EnvSinglePlayer.__init__(self, *args, **kwargs)
+        self.model = trained_model
 
     def embed_battle(self, battle):
-        """Embed Player to battles and return info from the Pokemon Showdown environment
-
-        Parameter
-        ----------
-        battle:
-            Max damage player's turn in the battle
-
-        Return
-        ----------
-        list of [4 moves, damaging values of each moves, number of pokemons of player, number of pokemons of opponent]
-        """
         # -1 indicates that the move does not have a base power
         # or is not available
         moves_base_power = -np.ones(4)
@@ -72,17 +60,6 @@ class TrainedRLPlayer(Gen8EnvSinglePlayer):
         )
 
     def compute_reward(self, battle) -> float:
-        """Compute rewards for each action taken of player in the battle
-
-        Parameter
-        ----------
-        battle:
-            Max damage player's turn in the battle
-
-        Return
-        ---------
-        float value of the rewards return from the environment
-        """
         return self.reward_computing_helper(
             battle, fainted_value=2, hp_value=1, victory_value=30
         )
@@ -114,29 +91,29 @@ class TrainedRLPlayer(Gen8EnvSinglePlayer):
         :rtype: str
         """
         if (
-            action < 4
-            and action < len(battle.available_moves)
-            and not battle.force_switch
+                action < 4
+                and action < len(battle.available_moves)
+                and not battle.force_switch
         ):
             return self.create_order(battle.available_moves[action])
         elif (
-            not battle.force_switch
-            and battle.can_z_move
-            and 0 <= action - 4 < len(battle.active_pokemon.available_z_moves)
+                not battle.force_switch
+                and battle.can_z_move
+                and 0 <= action - 4 < len(battle.active_pokemon.available_z_moves)
         ):
             return self.create_order(
                 battle.active_pokemon.available_z_moves[action - 4], z_move=True
             )
         elif (
-            battle.can_mega_evolve
-            and 0 <= action - 8 < len(battle.available_moves)
-            and not battle.force_switch
+                battle.can_mega_evolve
+                and 0 <= action - 8 < len(battle.available_moves)
+                and not battle.force_switch
         ):
             return self.create_order(battle.available_moves[action - 8], mega=True)
         elif (
-            battle.can_dynamax
-            and 0 <= action - 12 < len(battle.available_moves)
-            and not battle.force_switch
+                battle.can_dynamax
+                and 0 <= action - 12 < len(battle.available_moves)
+                and not battle.force_switch
         ):
             return self.create_order(battle.available_moves[action - 12], dynamax=True)
         elif 0 <= action - 16 < len(battle.available_switches):
@@ -145,14 +122,16 @@ class TrainedRLPlayer(Gen8EnvSinglePlayer):
             return self.choose_random_move(battle)
 
     def choose_move(self, battle):
-        if battle.available_moves:
+        if (battle.available_moves):
+            print("Available Move")
+            # if the player can attack, it will
             observations = self.embed_battle(battle)
-            action = self.model.predict(observations)[0]
+            action = self.model.predict(observations)[0] # this probablity wrong
+            # action = 5
             return self._action_to_move(action, battle)
         else:  # Take randome move
+            print("Random Move")
             return self.choose_random_move(battle)
-
-
 class MaxDamagePlayer(RandomPlayer):
     def choose_move(self, battle):
         """Choose the best move of the max damage player.
@@ -171,11 +150,13 @@ class MaxDamagePlayer(RandomPlayer):
         # If the player can attack, it will
         if battle.available_moves:
             # Finds the best move among available ones
+            print("Choose Move")
             best_move = max(battle.available_moves, key=lambda move: move.base_power)
             return self.create_order(best_move)
 
         # If no attack is available, a random switch will be made
         else:
+            print("Random Move")
             return self.choose_random_move(battle)
 
 
@@ -187,13 +168,13 @@ async def main():
     )
 
     # Sending challenges to "your_username"
-    await simplerl_player.send_challenges("mnguyen", n_challenges=1)
+    await simplerl_player.send_challenges("mnguyen_2", n_challenges=1)
 
     # Accepting one challenge from any user
     await simplerl_player.accept_challenges(None, 1)
 
     # Accepting three challenges from "your_username"
-    await simplerl_player.accept_challenges("mnguyen", 2)
+    await simplerl_player.accept_challenges("mnguyen_2", 2)
 
     # Playing 5 games on the ladder
     await simplerl_player.ladder(5)
